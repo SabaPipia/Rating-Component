92a7ff19b7d2b4508e126121c7cd6775c2e9471f
Ch34ry <pipiasaba788@gmail.com>
2023-06-07 20:25:49 +0400

    improve rating component

--- src/components/RatingNumbers.js
+++ src/components/RatingNumbers.js
@@ -8,11 +8,9 @@ function RatingNumbers(props) {
 
   return (
     <div className="rating">
-      <span onClick={() => handleRating(1)}>1</span>
-      <span onClick={() => handleRating(2)}>2</span>
-      <span onClick={() => handleRating(3)}>3</span>
-      <span onClick={() => handleRating(4)}>4</span>
-      <span onClick={() => handleRating(5)}>5</span>
+      {[1, 2, 3, 4, 5].map((rating) => {
+        return <span onClick={() => handleRating(rating)}>{rating}</span>;
+      })}
     </div>
   );
 }
--- src/components/RatingSection.js
+++ src/components/RatingSection.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import RatingNumbers from "./RatingNumbers";
 import Button from "./Button";
 
